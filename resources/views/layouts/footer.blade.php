f1e682a8233ed736527ca4b8a7ff2edd37465bf4
Mohamed Sobhi <mohaamed.sobhii@gmail.com>
2023-01-02 17:48:48 +0200

    first commit

--- resources/views/layouts/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.13.1/datatables.min.css"/>
- 
-<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.13.1/datatables.min.js"></script>
-    <script>
-        $(document).ready( function () {
-            $('#albums').DataTable();
-        } );
-    </script>  
